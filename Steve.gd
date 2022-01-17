63842c29e68d456d4f03377ede0e137a618b4e27
Lev <levoncii23@gmail.com>
2022-01-17 18:35:24 +0300

    upload v1.0

---
+++ Steve.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+const SPEED = 180
+const GRAVITY = 35
+const JUMPFORCE = -900
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+		$Sprite.play("walk")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+		$Sprite.play("walk")
+		$Sprite.flip_h = true
+	else:
+		$Sprite.play("idle")
+	
+	if not is_on_floor():
+		$Sprite.play("air")
+	
+	velocity.y = velocity.y + GRAVITY
+	
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMPFORCE
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	velocity.x = lerp(velocity.x, 0, 0.15)
+	
